74a413447c9539dcbdb4e7def6434b13a963c935
Sơn <sonngoc.gg@gmail.com>
2020-07-11 00:04:41 +0700

    first commit

---
+++ EJB02-ejb/src/java/session/UsersFacade.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session;
+
+import entity.Users;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Son
+ */
+@Stateless
+public class UsersFacade extends AbstractFacade<Users> implements UsersFacadeLocal {
+    @PersistenceContext(unitName = "EJB02-ejbPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public UsersFacade() {
+        super(Users.class);
+    }
+
+    @Override
+    public boolean checkLogin(String username, String password) {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Query query = em.createNamedQuery("Users.checkLogin");
+        query.setParameter("username", username);
+        query.setParameter("password", password);
+        
+        return query.getSingleResult() != null;
+    }
+    
+}
---
+++ EJB02-war/src/java/service/LoginWebService.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import javax.ejb.EJB;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import session.UsersFacadeLocal;
+
+/**
+ *
+ * @author Son
+ */
+@WebService(serviceName = "LoginWebService")
+public class LoginWebService {
+    @EJB
+    private UsersFacadeLocal usersFacade;
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+    
+    @WebMethod(operationName = "login")
+    public boolean login(@WebParam(name = "username") String username, @WebParam(name = "password") String password) {
+        return usersFacade.checkLogin(username, password);
+    }
+    
+    
+}
